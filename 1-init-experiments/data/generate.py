d24c44be506e9fccdddc3950ef1e8d4385453969
Marco Ravelo <mravelo5874@gmail.com>
2024-09-17 22:57:06 -0500

    added bunny graph

--- 1-init-experiments/data/generate.py
+++ 1-init-experiments/data/generate.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from typing import Tuple
 
 # * coords.shape: [x, 2] for 2D or [x, 3] in 3D (default to 3D)
@@ -17,6 +18,11 @@ def generate_square_plane_graph(n_points: int, length: float=1.0) -> Tuple[torch
     coords = torch.stack([x, y, z]).reshape(3, -1).permute(1, 0)
     dist = ((coords.unsqueeze(1) - coords.unsqueeze(0)) ** 2).sum(dim=-1).fill_diagonal_(torch.inf)
     edges = torch.argwhere(dist <= dist.min() + 0.001).T
+    return coords, edges
+
+def generate_bunny_graph() -> Tuple[torch.Tensor, torch.Tensor]:
+    coords = torch.tensor(np.load('data/bunny/bunny-coords.npy'))
+    edges = torch.tensor(np.load('data/bunny/bunny-edges.npy'))
     return coords, edges
 
 def generate_cube_graph():
