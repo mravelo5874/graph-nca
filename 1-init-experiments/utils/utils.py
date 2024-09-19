e564b9fc64d7112537afccac6b9e22e94a9c9c5c
Marco Ravelo <mravelo5874@gmail.com>
2024-09-18 23:33:34 -0500

    added nearest neighbor for graph computation

--- 1-init-experiments/utils/utils.py
+++ 1-init-experiments/utils/utils.py
@@ -1,35 +1,31 @@
-from typing import Optional
-import torch
 
-def pad3d(
-    tensor: torch.Tensor,
-    sizes: torch.LongTensor,
-    max_size: Optional[int] = None
-):
-    assert tensor.ndim == 2
-    assert tensor.size(0) == sizes.sum()
-    offset = [0] + torch.Tensor.tolist(sizes.cumsum(0))
-    max_n_nodes = sizes.max() if max_size is None else max_size
-    padded_tensor = torch.zeros(sizes.size(0), max_n_nodes, tensor.size(1)).to(tensor.device)
-    for i in range(sizes.size(0)):
-        padded_tensor[i][:sizes[i]] = tensor[offset[i]:offset[i+1]]
-    return padded_tensor
+import numpy as np
+from sklearn import neighbors
 
-def edge_index2adj_with_weight(
-    edge_index: torch.LongTensor,
-    edge_weight: torch.Tensor,
-    n_nodes: torch.LongTensor
-):
-    n_tot_nodes, n_max_nodes = n_nodes.sum(), n_nodes.max()
-    adj_ = torch.zeros(n_tot_nodes, n_tot_nodes, dtype=torch.uint8).to(n_nodes.device)
-    adj_[edge_index[0], edge_index[1]] = 1
-    adj_weight_ = torch.zeros(n_tot_nodes, n_tot_nodes).to(n_nodes.device)
-    adj_weight_[edge_index[0], edge_index[1]] = edge_weight
+def compute_connectivity(positions, radius, add_self_edges):
+    """Get the indices of connected edges with radius connectivity.
 
-    offset = torch.cat([torch.zeros(1, dtype=torch.long).to(n_nodes.device), n_nodes.cumsum(0)])
-    adj = torch.zeros(n_nodes.size(0), n_max_nodes, n_max_nodes, dtype=torch.uint8).to(n_nodes.device)
-    adj_weight = torch.zeros(n_nodes.size(0), n_max_nodes, n_max_nodes).to(n_nodes.device)
-    for i in range(n_nodes.size(0)):
-        adj[i][:n_nodes[i], :n_nodes[i]] = adj_[offset[i]:offset[i + 1], offset[i]:offset[i + 1]]
-        adj_weight[i][:n_nodes[i], :n_nodes[i]] = adj_weight_[offset[i]:offset[i + 1], offset[i]:offset[i + 1]]
-    return adj, adj_weight
+    Args:
+        positions: Positions of nodes in the graph. Shape:
+        [num_nodes_in_graph, num_dims].
+        radius: Radius of connectivity.
+        add_self_edges: Whether to include self edges or not.
+
+    Returns:
+        senders indices [num_edges_in_graph]
+        receiver indices [num_edges_in_graph]
+
+    """
+    tree = neighbors.KDTree(positions)
+    receivers_list = tree.query_radius(positions, r=radius)
+    num_nodes = len(positions)
+    senders = np.repeat(range(num_nodes), [len(a) for a in receivers_list])
+    receivers = np.concatenate(receivers_list, axis=0)
+
+    if not add_self_edges:
+        # Remove self edges.
+        mask = senders != receivers
+        senders = senders[mask]
+        receivers = receivers[mask]
+
+    return senders, receivers
